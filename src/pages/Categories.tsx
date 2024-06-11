aad175fe48604c01bec961e9953caa643247f8b3
unknown <nuhibesnik@gmail.com>
2024-06-11 14:16:39 +0200

    bug

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -2,7 +2,6 @@ import CategoriesItems from "../Components/categories/CategoriesItems";
 import Footer from "../Components/footer/Footer";
 import Header from "../Components/header/Header";
 import Newsletter from "../Components/newsletter/Newsletter";
-import styles from "./categories.module.css";
 
 export default function Categories() {
   return (
