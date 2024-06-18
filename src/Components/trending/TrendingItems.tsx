80f801304bd8b47bd37af7bb7af36d1468d3229f
unknown <nuhibesnik@gmail.com>
2024-06-18 17:39:12 +0200

    trending items fix

--- src/Components/trending/TrendingItems.tsx
+++ src/Components/trending/TrendingItems.tsx
@@ -1,21 +1,28 @@
 import styles from "./trendingitems.module.css";
-
 import { items } from "../../assets/AllData";
+import { Link } from "react-router-dom";
 
 export default function TrendingItems() {
   const filteredItems = items.filter((item) => item.id >= 8);
 
   return filteredItems.map((item) => (
     <div key={item.id} className={styles.item}>
-      <a href="">
+      <Link
+        onClick={() => {
+          if (window.top) {
+            window.top.scrollTo(0, 0);
+          }
+        }}
+        to={`/categories/product/${item.id}`}
+      >
         <div className={styles.imgContainer}>
-          <img src={item.img} alt="" />
+          <img src={item.img} alt="product" />
         </div>
         <div className={styles.itemDescription}>
           <p>{item.description}</p>
           <p className={styles.price}>${item.price}</p>
         </div>
-      </a>
+      </Link>
     </div>
   ));
 }
