b188356e96365d36823554d61d67b3043605ce83
unknown <nuhibesnik@gmail.com>
2024-06-20 16:41:33 +0200

    added cart

---
+++ src/Components/cart/Cart.tsx
@@ -0,0 +1,45 @@
+import styles from "./cart.module.css";
+import cartImg from "../../img/cart/empty-cart.png";
+
+export default function Cart({ toggle, active }) {
+  return (
+    <>
+      <div
+        onClick={toggle}
+        className={`${styles.pageOverlay} ${active ? styles.overActive : ""}`}
+      ></div>
+      <div className={`${styles.cartContainer} ${active ? styles.active : ""}`}>
+        <div className={styles.cartTitle}>
+          <h2>Your Shopping Cart (0)</h2>
+          <button onClick={toggle}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="tabler-icon tabler-icon-x"
+            >
+              <path d="M18 6l-12 12"></path>
+              <path d="M6 6l12 12"></path>
+            </svg>
+          </button>
+        </div>
+        <div className={styles.cartBody}>
+          <div className={styles.emptyCart}>
+            <img src={cartImg} alt="" />
+            <p>Your cart is empty</p>
+            <button className={styles.emptyButton} onClick={toggle}>
+              Keep Browsing
+            </button>
+          </div>
+          <div className={styles.fullCart}></div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -1,46 +1,59 @@
 import styles from "./header.module.css";
 import logo from "../../img/newlogo2.png";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import Cart from "../cart/Cart";
 
 export default function Header() {
+  const [active, setActive] = useState<boolean>(false);
+
+  const toggle = (e: any): void => {
+    e.preventDefault();
+    setActive(!active);
+  };
+
   return (
-    <nav className={styles.nav}>
-      <div className={styles.container}>
-        <div className={styles.navcontainer}>
-          <NavLink to="/">
-            <img className={styles.logo} src={logo} alt="logo" />
-          </NavLink>
-          <ul className={styles.navlist}>
-            <li>
-              <NavLink to="/categories">categories</NavLink>
-            </li>
-            <li>
-              <NavLink to="/categories/product/1">product page</NavLink>
-            </li>
-            <li>
-              <a href="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className=""
-                >
-                  <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                  <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-                  <path d="M17 17h-11v-14h-2"></path>
-                  <path d="M6 5l14 1l-1 7h-13"></path>
-                </svg>
-              </a>
-            </li>
-          </ul>
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.container}>
+          <div className={styles.navcontainer}>
+            <NavLink to="/">
+              <img className={styles.logo} src={logo} alt="logo" />
+            </NavLink>
+            <ul className={styles.navlist}>
+              <li>
+                <NavLink to="/categories">categories</NavLink>
+              </li>
+              <li>
+                <NavLink to="/categories/product/1">product page</NavLink>
+              </li>
+              <li>
+                <button onClick={toggle}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className=""
+                  >
+                    <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                    <path d="M17 17h-11v-14h-2"></path>
+                    <path d="M6 5l14 1l-1 7h-13"></path>
+                  </svg>
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <Cart toggle={toggle} active={active} />
+    </>
   );
 }
